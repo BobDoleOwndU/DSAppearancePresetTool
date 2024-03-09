94b31bdbf83f1b83a6a4809dc53873e562ccc314
BobDoleOwndU <spikey5556@hotmail.com>
2024-03-09 14:39:38 -0500

    Error handling and assembly info.

--- DSRAppearancePresetTool/MainForm.cs
+++ DSRAppearancePresetTool/MainForm.cs
@@ -12,29 +12,44 @@ namespace DSRAppearancePresetTool
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Character Appearance Preset|*.dsrchr";
-            saveFileDialog.Title = "Choose an output location";
-
-            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            try
             {
-                MemoryHandler.ReadAppearanceData().Write(saveFileDialog.FileName);
-            } //if
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Character Appearance Preset|*.dsrchr";
+                saveFileDialog.Title = "Choose an output location";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    MemoryHandler.ReadAppearanceData().Write(saveFileDialog.FileName);
+                } //if
+            } //try
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}\n{ex.StackTrace}");
+            } //catch
+            
         } //saveButton_Click
 
         private void loadButton_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Character Appearance Preset|*.dsrchr";
-            openFileDialog.Title = "Select the file to load";
-
-            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            try
             {
-                AppearanceData appearanceData = new AppearanceData();
-                appearanceData.Read(openFileDialog.FileName);
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Character Appearance Preset|*.dsrchr";
+                openFileDialog.Title = "Select the file to load";
 
-                MemoryHandler.WriteAppearanceData(appearanceData);
-            } //if
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    AppearanceData appearanceData = new AppearanceData();
+                    appearanceData.Read(openFileDialog.FileName);
+
+                    MemoryHandler.WriteAppearanceData(appearanceData);
+                } //if
+            } //try
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}\n{ex.StackTrace}");
+            } //catch
         } //loadButton_Click
     } //class
 } //namespace
--- DSRAppearancePresetTool/Properties/AssemblyInfo.cs
+++ DSRAppearancePresetTool/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DSRAppearancePresetTool")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © BobDoleOwndU 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
